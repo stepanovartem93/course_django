a5faa001597b25139df1ecd97f9b10f6b4cdd1d0
stepanovartem93 <45hfVfks213>
2019-10-15 13:46:27 +0300

    добавил placeholder'ы в location --- но слетел footer... нужно подправить....

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -7,8 +7,29 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 def contacts(request):
+    locations = [
+        {
+            'city': 'Москва',
+            'phone':  '+7 987 654 32 10',
+            'adress':  'ул. Ленина, д. 2',
+            'email':  'contact.msk@myshop.ru',
+        },
+        {
+            'city': 'Санкт-Петербург',
+            'phone':  '+7 987 654 32 10',
+            'adress':  'ул. Петровская наб., д. 3',
+            'email':  'contact.spb@myshop.ru',
+        },
+        {
+            'city': 'Екатеринбург',
+            'phone':  '+7 987 654 32 10',
+            'adress':  'ул. Б. Ордынка, д. 4',
+            'email':  'contact.ekb@myshop.ru', 
+        },
+    ]
     context = {
         'page_title':'контакты',
+        'locations':locations,
     }
     return render(request, 'mainapp/contacts.html', context)
 
