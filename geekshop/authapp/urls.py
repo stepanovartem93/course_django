e98ae92509725646f4bd525a1b8f307e5500bac3
stepanovartem93 <stepanovartem93@gmail.com>
2019-10-21 22:17:25 +0300

    добавил форму аутентификации добавил login.html, base.html (страницу аутентификации) в диспетчере authapp\urls.py прописал путь к странице аутентификации

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -13,17 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, re_path, include
-from django.conf import settings
-from django.conf.urls.static import static
+import authapp.views as authapp
+
+
+app_name = 'authapp'
+
 
 urlpatterns = [
-    re_path(r'^', include ('mainapp.urls', namespace='main')),
-    re_path(r'^auth/', include ('authapp.urls', namespace='auth')),
-    path('admin/', admin.site.urls),
+    re_path(r'^login/$', authapp.user_login, name='user_login'),
+    # re_path(r'^logout/$', authapp.user_logout, name='user_logout'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
