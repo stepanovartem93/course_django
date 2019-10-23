fae52696d4a8b6a2325244570032a1ef47443716
stepanovartem93 <45hfVfks213>
2019-10-23 15:20:56 +0300

    Подкорректировал БД Добавил новых пользователей Подправил форму регистрации новых пользоватлей

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -17,13 +17,13 @@ class ShopUserLoginForm(AuthenticationForm):
 class ShopUserRegisterForm(UserCreationForm):
     class Meta:
         model = ShopUser
-        fields = ('username', 'first_name', 'last_name', 'password2', 'email', 'age', 'avatar')
+        fields = ('username', 'password1', 'password2', 'first_name', 'email', 'age', 'avatar')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-            # field.help_text = ''
+            field.help_text = ''
 
     def clean_age(self):
         data = self.cleaned_data['age']
